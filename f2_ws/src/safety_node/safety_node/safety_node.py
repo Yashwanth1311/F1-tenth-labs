8d6512693852cda0fc00e84b40dbbfbe866330e5
Yashwanth <iamyeshwanth@gmail.com>
2024-11-29 16:44:40 +0530

    Updated for negative range rates

--- f2_ws/src/safety_node/safety_node/safety_node.py
+++ f2_ws/src/safety_node/safety_node/safety_node.py
@@ -25,13 +25,13 @@ class SafetyNode(Node):
 
         NOTE that the x component of the linear velocity in odom is the speed
         """
-        self.speed = 0.
+        self.speed=0.0
         # TODO: create ROS subscribers and publishers.
         self.odom_subsciption=self.create_subscription(Odometry,"ego_racecar/odom",self.odom_callback,10)
         self.scan_subsciption=self.create_subscription(LaserScan,"scan",self.scan_callback,10)
 
 
-        self.publisher = self.create_publisher(AckermannDriveStamped,"drive",1)
+        self.publisher = self.create_publisher(AckermannDriveStamped,"drive",10)
 
 
 
@@ -43,37 +43,34 @@ class SafetyNode(Node):
     def scan_callback(self, scan_msg):
         # TODO: calculate TTC
 
+        ranges=np.array(scan_msg.ranges)
+        ranges[ranges<scan_msg.range_min]=scan_msg.range_min
+        ranges[ranges>scan_msg.range_max]=scan_msg.range_max
+        angle_radians=np.arange(scan_msg.angle_min,scan_msg.angle_max,scan_msg.angle_increment)
+        range_rates=np.cos(angle_radians)*self.speed+1e-6
 
-        min_range=min(scan_msg.ranges)
-        min_range_index=np.argmin(scan_msg.ranges)
+        TTCs=np.array([ranges/range_rates])
+        TTCs[TTCs<0]=1e+6
 
-        min_range_angle_rad=scan_msg.angle_min+scan_msg.angle_increment*min_range_index
-
-        range_rate=self.speed*math.cos(min_range_angle_rad)
-
-
-        TTC=abs(min_range)/max(range_rate,0.0000000000001)
-
-        # print(f"min - {min_range}, angle - {min_range_angle_rad*180/np.pi} TTC - {TTC}")
  
-        # TODO: publish command to brake
-        if TTC<4:
+        # # TODO: publish command to brake
+        
+        if np.min(TTCs)<1.2:
             new_msg=AckermannDriveStamped()
-
             new_msg.drive.speed=0.0
-            print(f"min - {min_range}, angle - {min_range_angle_rad*180/np.pi} TTC - {TTC}")    
             # new_msg.drive.steering_angle=0.0
-
             self.publisher.publish(new_msg)        
-        elif TTC<8:
-            print(f"min - {min_range}, angle - {min_range_angle_rad*180/np.pi} TTC - {TTC}")
-            new_msg=AckermannDriveStamped()
+
+            print("BREAK!!!!")
+        # elif np.min(TTCs)<4:
+        
+        #     new_msg=AckermannDriveStamped()
 
 
-            new_msg.drive.speed=0.1
-            new_msg.drive.steering_angle=0.0
+        #     new_msg.drive.speed=0.1
+        #     new_msg.drive.steering_angle=0.0
 
-            self.publisher.publish(new_msg)
+        #     self.publisher.publish(new_msg)
 
         
 
